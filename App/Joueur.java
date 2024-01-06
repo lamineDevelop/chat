05b5240e4485d610aff61385ec2ba444f05d8b3b
lamineDevelop <123151625+lamineDevelop@users.noreply.github.com>
2024-01-06 23:19:26 +0100

    Update Joueur.java

--- App/Joueur.java
+++ App/Joueur.java
@@ -1,16 +1,28 @@
 package com.company;
 
 import java.io.Serializable;
+import java.net.Socket;
 
 public class Joueur implements Serializable {
     private String login;
     private int score;
     private int statut;
 
-    public Joueur(String login, int score, int statut) {
+    private Socket socket;
+
+    public Joueur(String login, int score, int statut,Socket socket) {
         this.login = login;
         this.score = score;
         this.statut = statut;
+        this.socket=socket;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
     }
 
     public Joueur() {
